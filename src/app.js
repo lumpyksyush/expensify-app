e6b0e748ddf978f3d5fee7a38408d5c710ffe950
lumpyksyush <lumpyksyush@gmail.com>
2020-04-10 19:46:44 +0300

    Removed dummy data

--- src/app.js
+++ src/app.js
@@ -11,18 +11,6 @@ import "./styles/styles.scss";
 
 const store = configureStore();
 
-const expenseOne = store.dispatch(addExpense({description: "Water bill", amount: 333500, createdAt: 312312310 }));
-const expenseTwo = store.dispatch(addExpense({description: "Gas bill", amount: 200, createdAt: 1000}));
-const expenseThree = store.dispatch(addExpense({description: "Rent", amount: 3, createdAt: 20000 }));
-
-//store.dispatch(setTextFilter('bill'));
-
-let state = store.getState();
-
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-
 // const App = () => (
 //     <Provider store = {store}>
 //         <AppRouter />
